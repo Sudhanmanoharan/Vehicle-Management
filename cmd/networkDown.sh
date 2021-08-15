56034ca4a6e7a64d169aba0bc3e44e590ea8ce12
Sudhan Manoharan <satrunalia.sudhan@gmail.com>
2021-08-15 18:26:03 +0530

    Hyperledger Fabric Version Upgrade ^2.3.0

--- cmd/networkDown.sh
+++ cmd/networkDown.sh
@@ -6,11 +6,17 @@
 #
 # Exit on first error
 set -ex
+mode="down"
+if [ $1 == "down" ]; then
+    mode="down"
+elif [ $1 == "restart" ]; then
+    mode="restart"
+fi
 
 # Bring the test network down
 pushd ../test-network
-./network.sh down
+./network.sh $mode
 popd
 
 # clean out any old identites in the wallets
-rm -rf ../api/fabcar/wallet/*
+rm -rf ../api/fabcar/javascript/wallet/*
